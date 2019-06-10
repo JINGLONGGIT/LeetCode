2b42453f047c0d670fc5abd1f1206b456dcdf4f2
DESKTOP-VQN03I8\景龙 <jinglong_email@163.com>
2019-06-10 19:40:51 +0800

    新增解法二：二分法

--- 035.搜索插入位置/Search Insert Position.cc
+++ 035.搜索插入位置/Search Insert Position.cc
@@ -1,3 +1,4 @@
+// 解法一：暴力解法，遍历整个数组
 class Solution 
 {
 public:
@@ -27,5 +28,36 @@ public:
         }
         
         return nums.size();
+    }
+};
+
+// 解法二：二分法
+class Solution 
+{
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int high = nums.size() - 1;
+        int low = 0;
+        
+        while (low <= high)
+        {
+            int mid = (high + low) / 2;
+            
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else 
+            {
+                low = mid + 1;
+            }
+        }
+        
+        return low;
     }
 };
