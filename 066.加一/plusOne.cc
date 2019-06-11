160ba184f5c896a072935d45ca51eee87109811a
DESKTOP-VQN03I8\景龙 <jinglong_email@163.com>
2019-06-11 20:05:47 +0800

    新增066加一解法一

---
+++ 066.加一/plusOne.cc
@@ -0,0 +1,43 @@
+// 解法一：
+class Solution 
+{
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+        int i32CarryFlag = 0;       // 进位标志位
+        
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            digits[i]++;
+            if (digits[i] == 10)
+            {
+                i32CarryFlag = 1;
+            }
+            else 
+            {
+                break;
+            }
+            
+            if (i32CarryFlag)
+            {
+                i32CarryFlag = 0;
+                digits[i] = 0;
+            }
+            
+            if (i == 0 && digits[i] == 0)
+            {
+                digits.push_back(0);
+
+                for (int i = digits.size() - 2; i >= 0; i--)
+                {
+                    digits[i + 1] = digits[i];
+                }
+                
+                digits[0] = 1;
+            }
+        }
+        
+        return digits;
+    }
+};
+
