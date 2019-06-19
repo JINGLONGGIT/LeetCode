319dcb6f82e076bfe5ce473ac968e8e60b4aa4fd
DESKTOP-VQN03I8\景龙 <jinglong_email@163.com>
2019-06-19 09:39:20 +0800

    second solution for #509 and modified README.md file

--- 509.斐波那契数列/FibonacciNumber.cc
+++ 509.斐波那契数列/FibonacciNumber.cc
@@ -18,3 +18,26 @@ public:
         }
     }
 };
+
+// 解法二：数组方式
+class Solution {
+public:
+    int fib(int N) 
+    {
+        int i32Tmp = 0;
+        vector<int> nums = {0, 1};
+        
+        if (N < 2)
+        {
+            return N;
+        }
+        
+        for (int i = 2; i <= N; i++)
+        {
+            i32Tmp = nums[i - 1] + nums[i - 2];
+            nums.push_back(i32Tmp);
+        }
+        
+        return i32Tmp;
+    }
+};
