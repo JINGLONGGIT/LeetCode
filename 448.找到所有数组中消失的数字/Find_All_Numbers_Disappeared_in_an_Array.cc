a51a6f6a33eba51876184072b5a2c700c5504244
DESKTOP-VQN03I8\景龙 <jinglong_email@163.com>
2019-07-02 17:08:02 +0800

    solution two for #448

--- 448.找到所有数组中消失的数字/Find_All_Numbers_Disappeared_in_an_Array.cc
+++ 448.找到所有数组中消失的数字/Find_All_Numbers_Disappeared_in_an_Array.cc
@@ -23,3 +23,25 @@ public:
     }
 };
 
+// 解法二：
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1]);
+        }
+        
+        vector<int> tmp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                tmp.push_back(i + 1);
+            }
+        }
+        
+        return tmp;
+    }
+};
