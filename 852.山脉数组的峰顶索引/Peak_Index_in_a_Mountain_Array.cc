3a4d463d3e126a7f11abe4eb44f74976874ff332
DESKTOP-VQN03I8\JINGLONG <jinglong_email@163.com>
2019-10-30 11:52:33 +0800

    新增#852二分法算法实现

---
+++ 852.山脉数组的峰顶索引/Peak_Index_in_a_Mountain_Array.cc
@@ -0,0 +1,23 @@
+// 二分法
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) 
+    {
+        int left = 0;
+        int right = A.size() - 1;
+        
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if ((A[mid] > A[mid - 1]) && (A[mid] > A[mid + 1]))   
+                return mid;
+            else if ((A[mid] < A[mid - 1]) && A[mid] > A[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        
+        return -1;
+    }
+};
