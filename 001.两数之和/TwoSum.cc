0f8fda0e2f4bde693913c2b21d92ea0645eaa98d
DESKTOP-VQN03I8\JINGLONG <jinglong_email@163.com>
2019-11-01 11:28:01 +0800

    修改：新增两遍哈希/一遍哈希算法C++实现

--- 001.两数之和/TwoSum.cc
+++ 001.两数之和/TwoSum.cc
@@ -1,3 +1,4 @@
+// 双指针/双重循环
 class Solution 
 {
 public:
@@ -27,5 +28,50 @@ public:
         }
         
         return vecResult;
+    }
+};
+
+// 两遍哈希表
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        map<int, int> HashMap;
+        vector<int> res;
+        
+        for (int i = 0; i < nums.size(); i++)
+            HashMap.insert(map<int, int>::value_type(nums[i], i));
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if ((HashMap.count(target - nums[i]) > 0) && (HashMap[target - nums[i]] != i))
+            {
+                res.push_back(i);
+                res.push_back(HashMap[target - nums[i]]);
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+// 一遍哈希表
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        map<int, int> HashMap;
+        vector<int> res;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if ((HashMap.count(target - nums[i]) > 0) && (HashMap[target - nums[i]] != i))
+            {
+                res.push_back(HashMap[target - nums[i]]);
+                res.push_back(i);
+                break;
+            }
+            HashMap.insert(map<int, int>::value_type(nums[i], i));
+        }
+        return res;
     }
 };
